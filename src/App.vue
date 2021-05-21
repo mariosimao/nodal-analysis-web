3bf891dc41b67d1b59f81336ffc409b589365236
Mario Gunter Simao <mario.simao@cern.ch>
2021-05-21 16:55:36 -0300

    Add basic interface for calculation

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,91 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-container fill-height>
+        <v-row align="center">
+          <v-col cols="5">
+            <v-textarea
+              v-model="netlist"
+              outlined
+              rows="20"
+              dense
+              no-resize
+              placeholder="Add your netlist here..."
+            />
+          </v-col>
+          <v-col
+            cols="2"
+          >
+            <v-btn
+              color="primary"
+              @click="calculate"
+            >
+              <v-icon left>mdi-calculator</v-icon>
+              Calculate
+            </v-btn>
+          </v-col>
+          <v-col cols="5">
+            <v-card>
+
+              <v-simple-table>
+                <tbody>
+                  <tr
+                    v-for="(resultLine, i) in result"
+                    :key="i"
+                  >
+                    <td><b>{{ resultLine.type}} {{ resultLine.name }}</b></td>
+                    <td>
+                      {{ formatValue(resultLine) }}
+                    </td>
+                  </tr>
+                </tbody>
+              </v-simple-table>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import { CircuitFactory } from '@mariosimao/nodal-analysis-core';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    netlist: '',
+    result: [],
   }),
+
+  methods: {
+    calculate() {
+      const circuit = CircuitFactory.fromNetlist(this.netlist);
+
+      this.result = circuit.nodalAnalysis();
+    },
+    formatValue(resultLine) {
+      const { re, im } = resultLine.value;
+      const { unit } = resultLine;
+
+      if (re === 0 && im === 0) {
+        return `0 ${unit}`;
+      }
+
+      if (re === 0) {
+        return `${im.toFixed(3)}i ${unit}`;
+      }
+
+      if (im === 0) {
+        return `${re.toFixed(3)} ${unit}`;
+      }
+
+      return `${re} ${im}i ${unit}`;
+    },
+  },
 };
 </script>
